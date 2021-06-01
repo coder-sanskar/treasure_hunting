6fb4999d55f44cb493d49184378ebcee23857825
coder-sanskar <74972821+coder-sanskar@users.noreply.github.com>
2021-06-01 23:42:13 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "ACADEMIC";
+const accessCode3 = "HOSPITAL";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C A A M E I C D", 700,160)
+    fill("lightblue")
+    text("Hint: It's related to education !!", 700,180)
+
+    fill("white")
+    textSize(15)
+    text("O I L H P T A S", 100,270)
+    fill("lightblue")
+    text("HINT: You probably go here when you are ill !!", 100,285)
+    
+
+}
